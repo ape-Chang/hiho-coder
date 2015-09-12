d93fce28acb45e2198d21adbd35f585c9bb1ba9f
ape.chang <ape.chang.1989@gmail.com>
2015-09-12 22:11:16 +0800

    f9096fc2-4b73-4e9e-a943-0e2726deab0c

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -1,84 +1,66 @@
 package ape.chang;
 
 import java.io.FileInputStream;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
   
-  static class DAG {
-    static class Node {
-      public int id;
-      public List<Node> jacent;
-      public Node(int id) {
-        this.id = id;
-        this.jacent = new ArrayList<Node>();
-      }
-      // extra
-      public int virus;
-      public int indegree; 
-    };
-    
-    public List<Node> nodes;
-    
-    public void addEdge(int from, int to) {
-      nodes.get(to-1).indegree++;
-      nodes.get(from-1).jacent.add(nodes.get(to-1));
+  static class M2x2{
+    public long a1, a2, a3, a4;
+    public M2x2(long a1, long a2, long a3, long a4) {
+      this.a1 = a1;
+      this.a2 = a2;
+      this.a3 = a3;
+      this.a4 = a4;
     }
-    
-    public DAG(int n) {
-      nodes = new ArrayList<Main.DAG.Node>();
-      for (int i = 1; i <= n; ++i) 
-        nodes.add(new Node(i));
+    public M2x2(M2x2 mat) {
+      a1 = mat.a1; 
+      a2 = mat.a2;
+      a3 = mat.a3;
+      a4 = mat.a4;
     }
-    
-    public void topoSort() {
-      Queue<Node> noIndegrees = new LinkedList<Node>();
-      for (Node node : nodes) 
-        if (node.indegree == 0)
-          noIndegrees.offer(node);
-      
-      while (!noIndegrees.isEmpty()) {
-        Node node = noIndegrees.poll();
-        for (Node jacent : node.jacent) {
-          jacent.virus += node.virus;
-          if (jacent.virus >= 142857) jacent.virus %= 142857;
-          if (--jacent.indegree == 0) noIndegrees.add(jacent); 
-        }
-      }
+    public void multiply(M2x2 mat) {
+      long b1 = a1 * mat.a1 + a2 * mat.a3;
+      long b2 = a1 * mat.a2 + a2 * mat.a4;
+      long b3 = a3 * mat.a1 + a4 * mat.a3;
+      long b4 = a3 * mat.a2 + a4 * mat.a4;
+      a1 = b1%19999997; a2 = b2%19999997; a3 = b3%19999997; a4 = b4%19999997;
     }
-  };
+  }
 
+  public static long control(long n) {
+    long a = 0, b = 1;
+    while (n-- > 0) {
+      b = a+b;
+      a = b-a;
+      if (a > 19999997) a %= 19999997;
+      if (b > 19999997) b %= 19999997;
+    }
+    return b;
+  }
+  
   public static void main(String[] args) {
     try{System.setIn(new FileInputStream("input"));}catch(Exception e){return;}
     Scanner scanner = new Scanner(System.in);
-    
-    int n = scanner.nextInt();
-    DAG dag = new DAG(n);
-    
-    int m = scanner.nextInt();
-    int k = scanner.nextInt();
-    while (k-- > 0) {
-      DAG.Node node = dag.nodes.get(scanner.nextInt()-1);
-      node.virus = (++node.virus >= 142857 ? node.virus%142857 : node.virus);
+    long n = scanner.nextLong();
+    List<M2x2> expo = new ArrayList<Main.M2x2>();
+    expo.add(new M2x2(0, 1, 1, 1));
+    for (int i = 0; i < 64; ++i) {
+      M2x2 mat = new M2x2(expo.get(i));
+      mat.multiply(expo.get(i));
+      expo.add(mat);
     }
-    while (m-- > 0) {
-      int from = scanner.nextInt();
-      int to = scanner.nextInt();
-      dag.addEdge(from, to);
-    } 
+    M2x2 ret = new M2x2(1, 0, 0, 1);
+    for (int i = 0; n != 0; ++i, n >>= 1) 
+      if ((0x1 & n) == 1) 
+        ret.multiply(expo.get(i));
+      
+    System.out.println(ret.a4);
+    
     scanner.close();
-    dag.topoSort();
-    
-    int virus = 0;
-    for (DAG.Node node : dag.nodes) {
-      virus += node.virus;
-      if (virus >= 142857) virus %= 142857;
-    }
-    System.out.println(virus);
   }
   
 }
