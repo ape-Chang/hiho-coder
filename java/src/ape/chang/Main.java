bad1c1219a8d812781ac6a7b19cd54ada797a184
ape.chang <ape.chang.1989@gmail.com>
2015-09-12 15:00:10 +0800

    01817cf6-0f89-412b-b365-6d6d0d79580e

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -1,13 +1,15 @@
 package ape.chang;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
+
+import ape.chang.Main.DAG.Node;
 
 public class Main {
   
@@ -16,7 +18,6 @@ public class Main {
       public int id;
       public int virus;
       public int indegree;
-      public int outdegree;
       public Node(int id) {this.id = id;}
     };
     static class Edge {
@@ -26,26 +27,26 @@ public class Main {
     };
     
     private List<Node> nodes;
-    private Set<Edge> edges;
-    private Map<Integer, List<Node>> edges1;
+    private Map<Integer, List<Node>> edges;
     
     public void addEdge(int from, int to) {
-      nodes.get(from-1).outdegree++;
       nodes.get(to-1).indegree++;
-      edges.add(new Edge(nodes.get(from-1), nodes.get(to-1)));
+      edges.get(from).add(nodes.get(to-1));
     }
     
     public void setNodeNumber(int n) {
       nodes.clear();
-      for (int i = 1; i <= n; ++i)
+      for (int i = 1; i <= n; ++i) {
         nodes.add(new Node(i));
+        edges.put(i, new ArrayList<Node>());
+      }
     }
     
     public Node getNode(int id) {return nodes.get(id-1);}
     
     public DAG() {
       nodes = new ArrayList<Main.DAG.Node>();
-      edges = new HashSet<Main.DAG.Edge>();
+      edges = new HashMap<Integer, List<Node>>();
     }
     
     public void topoSort() {
@@ -54,19 +55,27 @@ public class Main {
       for (Node node : nodes) 
         if (node.indegree == 0)
           noIndegrees.offer(node);
+      
       while (!noIndegrees.isEmpty()) {
         Node node = noIndegrees.poll();
         sorted.add(node);
-        for (Edge edge : edges)
-          if (node == edge.from) 
-            if (--edge.to.indegree == 0)
-              noIndegrees.offer(edge.to);
+        for (Node jacent : edges.get(node.id)) 
+          if (--jacent.indegree == 0)
+            noIndegrees.add(jacent);
       }
+      
+      for (Integer id : edges.keySet()) 
+        for (Node jacent : edges.get(id))
+          jacent.indegree++;
+      
+      nodes = sorted;
     }
   };
 
   public static void main(String[] args) {
+    try {System.setIn(new FileInputStream("input"));} catch (Exception e) {return;}
     Scanner scanner = new Scanner(System.in);
+    
     DAG dag = new DAG();
     int m = scanner.nextInt();
     int n = scanner.nextInt();
@@ -74,8 +83,20 @@ public class Main {
     dag.setNodeNumber(n);
     while (k-- > 0) dag.getNode(scanner.nextInt()).virus++;
     while (m-- > 0) dag.addEdge(scanner.nextInt(), scanner.nextInt());
-    dag.topoSort();
     scanner.close();
+    dag.topoSort();
+    
+    for (Node node : dag.nodes) 
+      for (Node jacent : dag.edges.get(node.id))
+        jacent.virus += node.virus;
+    
+    int virus = 0;
+    for (Node node : dag.nodes) {
+      virus += node.virus;
+      if (virus >= 142857) 
+        virus %= 142857;
+    }
+    System.out.println(virus);
   }
   
 }
