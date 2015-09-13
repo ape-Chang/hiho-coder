ac784f71f3a370fc6beb86bb17dfe2f5005d8026
ape.chang <ape.chang.1989@gmail.com>
2015-09-14 00:21:40 +0800

    706ed0c8-5ef7-4080-8e5f-89e9ab62c7cf

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -19,70 +19,120 @@ public class Main {
       this.salary = salary;
     }
   }
-  
-  public static List<List<Candidate>> combine(List<Candidate> all, int from, int size) {
+   
+  public static List<List<Candidate>> combine(List<Candidate> all, int k) {
     List<List<Candidate>> result = new ArrayList<List<Candidate>>();
-    if (all.size() == from + size) {
-      List<Candidate> list = new ArrayList<Candidate>();
-      for(int i = 0; i < size; ++i)
-        list.add(all.get(from+i));
-    } else {
-      result.addAll(combine(all, from+1, size));
-      for (List<Candidate> list : combine(all, from+1, size-1)) {
-        list.add(all.get(from));
-        result.add(list);
+    if (k == 0) return result;
+    
+    int n = all.size();
+    int[] index = new int[k];
+    for (int i = 0; i < index.length; ++i) index[i] = -1;
+    index[0] = 0;
+    int current = 1;
+    while (true) {
+      if (current == -1) break;
+      if (current == k) {
+        
+        List<Candidate> combination = new ArrayList<Candidate>();
+        for (int i = 0; i < k; ++i) combination.add(all.get(index[i]));
+        result.add(combination);
+        
+        --current;
+      } else {
+        if (index[current] == -1) {
+          index[current] = index[current-1] + 1;
+          ++current;
+        } else {
+          if (index[current] + 1 > n-k+current) {
+            index[current] = -1;
+            --current;
+          } else {
+            index[current]++;
+            ++current;
+          }
+        }
       }
     }
+    
     return result;
   }
   
+  public static List<Candidate> merge(List<Candidate> female, List<Candidate> male) {
+      List<Candidate> result = new ArrayList<Candidate>();
+      int i = 0, j = 0;
+      while (i < female.size() && j < male.size()) {
+	  Candidate f = female.get(i);
+	  Candidate m = male.get(j);
+	  if (f.id < m.id) {
+	      result.add(f);
+	      ++i;
+	  } else {
+	      result.add(m);
+	      ++j;
+	  }
+      }
+      while (i < female.size()) {
+	  result.add(female.get(i));
+	  ++i;
+      }
+      while (j < male.size()) {
+	  result.add(male.get(j));
+	  ++j;
+      }
+      return result;
+  }
+  
+  public static List<List<Candidate>> crossProduct(List<List<Candidate>> female, List<List<Candidate>> male) {
+      if (female.size() == 0) return male;
+      if (male.size() == 0) return female;
+      
+      List<List<Candidate>> result = new ArrayList<List<Candidate>>();
+      for (int i = 0; i < female.size(); ++i)
+	  for (int j = 0; j < male.size(); ++j)
+	      result.add(merge(female.get(i), male.get(j)));
+      return result;
+  }
+  
   public static void solve(List<Candidate> female, List<Candidate> male, int budget, int x, int y) {
-    List<List<Candidate>> femaleCombination = combine(female, 0, x);
-    List<List<Candidate>> maleCombination = combine(male, 0, y);
-    
+      int minSalary = 0;
     int maxAbility = 0;
-    int minSalary = 0;
     List<List<Candidate>> possibilities = new ArrayList<List<Candidate>>();
-    for (int i = 0; i < femaleCombination.size(); ++i)
-      for (int j = 0; j < maleCombination.size(); ++j) {
+    for (List<Candidate> comb : crossProduct(combine(female, y), combine(male, x))) {
         int ability = 0;
         int salary = 0;
-        List<Candidate> possibility = new ArrayList<Candidate>();
-        for(Candidate candidate : femaleCombination.get(i)) {
-          ability += candidate.ability;
-          salary += candidate.salary;
-          possibility.add(candidate);
+        for (Candidate candidate : comb) {
+            salary += candidate.salary;
+            ability += candidate.ability;
         }
-        for (Candidate candidate : maleCombination.get(j)) {
-          ability += candidate.ability;
-          salary += candidate.salary;
-          possibility.add(candidate);
-        }
+
         if (salary > budget) continue;
         if (ability == maxAbility) {
-          if (salary < minSalary) possibilities.clear();
-          possibilities.add(possibility);
+            if (salary > minSalary)
+        	continue;
+            if (salary == minSalary)
+        	possibilities.add(comb);
+            if (salary < minSalary) {
+        	minSalary = salary;
+        	possibilities.clear();
+        	possibilities.add(comb);
+            }
         }
         if (ability > maxAbility) {
-          possibilities.clear();
-          possibilities.add(possibility);
-        }
-      }
+            minSalary = salary;
+            maxAbility = ability;
+            possibilities.clear();
+            possibilities.add(comb);
+        }	
+    }
     
     System.out.println(String.format("%d %d", maxAbility, minSalary));
     
-    for (List<Candidate> possibility : possibilities) 
-      Collections.sort(possibility, new Comparator<Candidate>() {
-        @Override
-        public int compare(Candidate lhs, Candidate rhs) {
-          return rhs.id - lhs.id;
-        }});
-    
+    assert possibilities.size() != 0;
     Collections.sort(possibilities, new Comparator<List<Candidate>>(){
       @Override
       public int compare(List<Candidate> lhs, List<Candidate> rhs) {
         for (int i = 0; i < lhs.size(); ++i) {
-          if (lhs.get(i).id < rhs.get(i).id)
+          if (lhs.get(i).id > rhs.get(i).id)
             return 1;
           if (lhs.get(i).id < rhs.get(i).id)
             return -1;
@@ -91,9 +141,8 @@ public class Main {
       }});
     
     StringBuffer buffer = new StringBuffer();
-    for (Candidate candidate : possibilities.get(0)) {
+    for (Candidate candidate : possibilities.get(0)) 
       buffer.append(candidate.id).append(" ");
-    }
     System.out.println(buffer.toString());
   }
   
@@ -101,8 +150,8 @@ public class Main {
     try{System.setIn(new FileInputStream("input"));}catch(Exception e){return;}
     Scanner scanner = new Scanner(System.in);
     int N = scanner.nextInt();
-    int X = scanner.nextInt();
-    int Y = scanner.nextInt();
+    int X = scanner.nextInt(); // x == 0?
+    int Y = scanner.nextInt(); // y == 0?
     int B = scanner.nextInt();
     List<Candidate> female = new ArrayList<Main.Candidate>();
     List<Candidate> male = new ArrayList<Main.Candidate>();
@@ -110,7 +159,7 @@ public class Main {
       String gender = scanner.next();
       int ability = scanner.nextInt();
       int salary = scanner.nextInt();
-      if (gender == "F")
+      if (gender.equals("F"))
         female.add(new Candidate(i, ability, salary));
       else 
         male.add(new Candidate(i, ability, salary));
