737cf890888d25ad95c10c06d29c4e3d3ce500ab
ape.chang <ape.chang.1989@gmail.com>
2015-09-14 16:06:00 +0800

    a108566d-afa5-4ecb-92b1-ba3b65a53432

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -1,73 +1,106 @@
 package ape.chang;
 
 import java.io.FileInputStream;
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     
-    static class Dag {
+    static class DirectedGraph {
 	static class Node {
-	    public int id;
-	    public List<Node> jacent;
+	    int id;
+	    Node[] jacents;
 	    public Node(int id) {
 		this.id = id;
-		this.jacent = new ArrayList<Main.Dag.Node>();
+		jacents = new Node[10];
 	    }
-	    public int indegree;
 	}
-	public List<Node> nodes;
-	public Dag(int n) {
-	    nodes = new ArrayList<Main.Dag.Node>();
-	    for (int i = 0; i < n; ++i) {
-		nodes.add(new Node(i));
-	    }
+	List<Node> nodes;
+	public DirectedGraph() {
+	    nodes = new ArrayList<Node>();
+	    init();
 	}
 	public void addEdge(int from, int to) {
-	    nodes.get(from).jacent.add(nodes.get(to));
-	    nodes.get(to).indegree++;
+	    nodes.get(from).jacents[to] = nodes.get(to);
 	}
-	public boolean validate() {
-	    List<Node> sorted = new ArrayList<Node>();
-	    Queue<Node> noIndegrees = new LinkedList<Node>();
-	    for (Node node : nodes)
-		if (node.indegree == 0)
-		    noIndegrees.offer(node);
-	    while (!noIndegrees.isEmpty()) {
-		Node node = noIndegrees.poll();
-		sorted.add(node);
-		for (Node jacent : node.jacent) 
-		    if (--jacent.indegree == 0)
-			noIndegrees.offer(jacent);
+	public void init() {
+	    for (int i = 0; i < 10; ++i)
+		nodes.add(new Node(i));
+	    for (int i = 0; i < 10; ++i)
+		addEdge(i, i);
+	    
+	    addEdge(1, 2);
+	    addEdge(1, 3);
+	    addEdge(1, 4);
+	    addEdge(1, 5);
+	    addEdge(1, 6);
+	    addEdge(1, 7);
+	    addEdge(1, 8);
+	    addEdge(1, 9);
+	    addEdge(1, 0);
+	    
+	    addEdge(2, 3);
+	    addEdge(2, 5);
+	    addEdge(2, 6);
+	    addEdge(2, 8);
+	    addEdge(2, 9);
+	    addEdge(2, 0);
+	    
+	    addEdge(3, 6);
+	    addEdge(3, 9);
+	    
+	    addEdge(4, 5);
+	    addEdge(4, 6);
+	    addEdge(4, 7);
+	    addEdge(4, 8);
+	    addEdge(4, 9);
+	    addEdge(4, 0);
+	    
+	    addEdge(5, 6);
+	    addEdge(5, 8);
+	    addEdge(5, 9);
+	    addEdge(5, 0);
+	    
+	    addEdge(6, 9);
+	    
+	    addEdge(7, 8);
+	    addEdge(7, 9);
+	    addEdge(7, 0);
+	    
+	    addEdge(8, 9);
+	    addEdge(8, 0);
+	}
+	public int invalidPosition(String s) {
+	    Node node = nodes.get(s.charAt(0) - '0');
+	    int i = 1;
+	    while (i < s.length()) {
+		node = node.jacents[s.charAt(i) - '0'];
+		if (node == null) break;
+		else ++i;
 	    }
-	    if (sorted.size() == nodes.size()) return true;
-	    else return false;
+	    return i;
 	}
     }
     
     public static void main(String[] args) {
 	try{System.setIn(new FileInputStream("input"));}catch(Exception e){return;}
 	Scanner scanner = new Scanner(System.in);
+	DirectedGraph pad = new DirectedGraph();
+	BigInteger one = new BigInteger("1");
 	int t = scanner.nextInt();
 	while (t-- > 0) {
-	    int n = scanner.nextInt();
-	    Dag dag = new Dag(n);
-	    int m = scanner.nextInt();
-	    while (m-- > 0) {
-		int from = scanner.nextInt()-1;
-		int to = scanner.nextInt()-1;
-		dag.addEdge(from, to);
+	    String s = scanner.next();
+	    int p;
+	    while ((p = pad.invalidPosition(s)) != s.length()) {
+		char[] A = s.toCharArray();
+		while (++p < A.length) A[p] = '0';
+		s = new BigInteger(new String(A)).subtract(one).toString();
 	    }
-	    if (dag.validate())
-		System.out.println("Correct");
-	    else 
-		System.out.println("Wrong");
+	    System.out.println(s);
 	}
 	scanner.close();
     }
 
-    
 }
