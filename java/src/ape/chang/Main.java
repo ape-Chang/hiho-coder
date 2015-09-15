142ab578956d9ac7c6710e25ec0cbcee0a9bfedb
ape.chang <ape.chang.1989@gmail.com>
2015-09-16 07:54:54 +0800

    cf0a43e6-fa91-4345-87a5-2b331dc2cfc7

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -1,110 +1,47 @@
 package ape.chang;
 
 import java.io.FileInputStream;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.Stack;
 
 public class Main {
     
-    static class UGraph {
-	public int[][] M;
-	public int[][] dist;
-	public UGraph(int n) {
-	    M = new int[n][n];
-	    dist = new int[n][n];
-	    for (int i = 0; i < n; ++i)
-		for (int j = 0; j < n; ++j)
-		    if (i == j)
-			M[i][j] = dist[i][j] = 0;
-		    else
-			M[i][j] = dist[i][j] = Integer.MAX_VALUE;
-	}
-	public void addEdge(int from, int to, int length) {
-	    M[to][from] = M[from][to] = dist[from][to] = dist[to][from] = Math.min(M[from][to], length);
-	}
-	public int distance(int src, int dst) {
-	    if (src == dst) return 0;
-	    int n = M.length;
-	    
-	    int[] dist = new int[n];
-	    for (int i = 0; i < dist.length; ++i)
-		dist[i] = Integer.MAX_VALUE;
-	    dist[src] = 0;
-	    Set<Integer> visited = new HashSet<Integer>();
-
-	    while (n-- > 0) {
-		int x = 0, m = Integer.MAX_VALUE;
-		for (int y = 0; y < dist.length; ++y)
-		    if (!visited.contains(y) && dist[y] <= m)
-			m = dist[x = y];
-		visited.add(x);
-		for (int y = 0; y < dist.length; ++y)
-		    
-		    if (M[x][y] == Integer.MAX_VALUE || dist[x] == Integer.MAX_VALUE)
-			;
-		    else
-			dist[y] = Math.min(dist[y], dist[x] + M[x][y]);
+    private static int count9076(String s) {
+	Stack<Character> stack = new Stack<Character>();
+	for (char c : s.toCharArray())
+	    if (c == '9' || c == '0' || c == '7' || c == '6') 
+		stack.push(c);
+	int count = 0;
+	while (!stack.isEmpty()) {
+	    Stack<Character> _stack = new Stack<Character>();
+	    String pattern = "6079";
+	    int k = 0;
+	    while (!stack.isEmpty()) {
+		char c = stack.pop();
+		if (c == pattern.charAt(k))
+		    k++;
+		else
+		    _stack.push(c);
+		if (k == 4) {
+		    ++count;
+		    while (!_stack.isEmpty()) stack.push(_stack.pop());
+		    break;
+		}
 	    }
-	    return dist[dst];
 	}
-	private void computeDistance(int src) {
-	    int n = M.length;
-	    
-	    int[] dist = new int[n];
-	    for (int i = 0; i < dist.length; ++i)
-		dist[i] = Integer.MAX_VALUE;
-	    dist[src] = 0;
-	    Set<Integer> visited = new HashSet<Integer>();
-
-	    while (n-- > 0) {
-		int x = 0, m = Integer.MAX_VALUE;
-		for (int y = 0; y < dist.length; ++y)
-		    if (!visited.contains(y) && dist[y] <= m)
-			m = dist[x = y];
-		visited.add(x);
-		for (int y = 0; y < dist.length; ++y)
-		    
-		    if (M[x][y] == Integer.MAX_VALUE || dist[x] == Integer.MAX_VALUE)
-			;
-		    else
-			dist[y] = Math.min(dist[y], dist[x] + M[x][y]);
-	    }
-	    for (int i = 0; i < dist.length; ++i) 
-		this.dist[src][i] = this.dist[i][src] = dist[i];	    
-	}
-	private void Floyd() {
-	    int n = M.length;
-	    for (int k = 0; k < n; ++k)
-		for (int i = 0; i < n; ++i)
-		    for (int j = 0; j < n; ++j)
-			if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE)
-			    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
-	}
-	public void computeDistance() {
-	    Floyd();
-	}
+	return count;
     }
-    
+           
     public static void main(String[] args) {
 	try{System.setIn(new FileInputStream("input"));}catch(Exception e){return;}
 	Scanner scanner = new Scanner(System.in);
-	int n = scanner.nextInt();
-	int m = scanner.nextInt();
-	UGraph graph = new UGraph(n);
-	while (m-- > 0) {
-	    int from = scanner.nextInt() - 1;
-	    int to = scanner.nextInt() - 1;
-	    int length = scanner.nextInt();
-	    graph.addEdge(from, to, length);
-	}
-	graph.computeDistance();
-	for (int i = 0; i < n; ++i) {
-	    for (int j = 0; j < n; ++j)
-		System.out.print("" + graph.dist[i][j] + " ");
-	    System.out.println();
+	int t = Integer.valueOf(scanner.nextLine());
+	while (t-- > 0) {
+	    String s = scanner.next();
+	    System.out.println(count9076(s));
 	}
 	scanner.close();
     }
+
 }
 
