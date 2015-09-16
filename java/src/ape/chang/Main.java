f9b271b68684b8e7f8d8281e3d6adb533afc54ed
ape.chang <ape.chang.1989@gmail.com>
2015-09-16 13:35:35 +0800

    4e3b1c9c-e5a2-4f11-89ad-32d7a533888c

--- java/src/ape/chang/Main.java
+++ java/src/ape/chang/Main.java
@@ -8,17 +8,28 @@ public class Main {
     public static void main(String[] args) {
 	try{System.setIn(new FileInputStream("input"));}catch(Exception e){return;}
 	Scanner scanner = new Scanner(System.in);
-	int n = Integer.valueOf(scanner.nextLine());
-	int[] A = new int[n+1];
-	for (int i = 1; i <= n; ++i) {
-	    for (int j = i; j >= 1; --j) {
-		int x = scanner.nextInt();
-		A[j] = Math.max(A[j-1]+x, A[j]+x);
+	int n = scanner.nextInt();
+	int k = 0;
+	double d = Double.MAX_VALUE;
+	for(int i = 1; i <= n; ++i) {
+	    int a = scanner.nextInt();
+	    String op = scanner.next();
+	    int b = scanner.nextInt();
+	    double x = 0;
+	    if (op.equals("+")) 
+		x = Math.abs(a+b-9.0);
+	    else if (op.equals("-"))
+		x = Math.abs(a-b-9.0);
+	    else if (op.equals("*"))
+		x = Math.abs(1.0*a*b-9.0);
+	    else 
+		x = Math.abs(1.0*a/b-9.0);
+	    if (x < d) {
+		k = i;
+		d = x;
 	    }
 	}
-	int max = 0;
-	for (int i : A) if (i > max) max = i;
-	System.out.println(max);
+	System.out.println(k);
 	scanner.close();
     }
 
