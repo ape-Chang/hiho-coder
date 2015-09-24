b5e4855c2134bed0dac88b3e38e25a0b77ae373d
yijiayue <yijiayue@10.188.235.62>
2015-09-24 10:44:49 +0800

    使用背包方法解决均分问题

--- java/src/yjy/hiho/Dijkstra.java
+++ java/src/yjy/hiho/Dijkstra.java
@@ -1,5 +1,7 @@
 package yjy.hiho;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
@@ -32,9 +34,19 @@ public class Dijkstra {
 
 	// 实现Dijkstra算法寻找单源最短路径
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
+		String path = "/Users/yijiayue/Desktop/test2.in";
+		FileInputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(path);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		Scanner scan = new Scanner(inputStream, "UTF-8");
+		
 		int N = scan.nextInt();
 		int M = scan.nextInt();
+		int start = scan.nextInt();
+		int end = scan.nextInt();
 		Node[] nodes = new Node[N];
 		for(int i=0;i<N;i++){
 			nodes[i] = new Node(i);
@@ -42,8 +54,8 @@ public class Dijkstra {
 		}
 		
 		for(int i=0;i<M;i++){
-			int a = scan.nextInt()-1;
-			int b = scan.nextInt()-1;
+			int a = scan.nextInt();
+			int b = scan.nextInt();
 			int d = scan.nextInt();
 			Edge Ea = new Edge();
 			Ea.to = nodes[b];
@@ -55,7 +67,7 @@ public class Dijkstra {
 			nodes[b].adj.add(Eb);
 		}
 		PriorityQueue<Node> Q = new PriorityQueue<Node>();
-		Node firstNode = nodes[0];
+		Node firstNode = nodes[start];
 		firstNode.flag = true;
 		firstNode.distance = 0;
 		for(Edge e : firstNode.adj){
@@ -63,21 +75,23 @@ public class Dijkstra {
 			Q.add(e.to);
 		}
 		while(!Q.isEmpty()){
+//			System.out.println(Q.size());
 			Node n = Q.poll();
 			n.flag = true;
 			for(Edge e : n.adj){
 				if(!e.to.flag){
 					if(e.to.distance>n.distance+e.d){
 						e.to.distance = n.distance+e.d;
+						Q.add(e.to);
 					}
-					Q.add(e.to);
 				}
 			}
 		}
 		
-		for(int i=0;i<N;i++){
-			System.out.println(nodes[i].distance);
-		}
+//		for(int i=0;i<N;i++){
+//			System.out.println(nodes[i].distance);
+//		}
+		System.out.println(nodes[end].distance);
 		scan.close();
 		
 	}
